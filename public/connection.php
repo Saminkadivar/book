05e4e97a9ba715e61ff0da7ad03a7c7d32624e4e
Saminkadivar <saminkadivar2911@gmail.com>
2025-06-11 15:59:10 +0530

    Update connection.php

--- public/connection.php
+++ public/connection.php
@@ -1,7 +1,7 @@
 <?php
 try {
-    $conn = new PDO("pgsql:host=" . getenv('dpg-d14lgd6uk2gs73b0rr50-a') . ";port=" . getenv('5432') . ";dbname=" . getenv('mini_project_i0kw'), getenv('mini_project_i0kw_user'), getenv('1Sc7e6YK7bWVeadJLOByKPpQjQgrOOND'));
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $con = new PDO("pgsql:host=" . getenv('dpg-d14lgd6uk2gs73b0rr50-a') . ";port=" . getenv('5432') . ";dbname=" . getenv('mini_project_i0kw'), getenv('mini_project_i0kw_user'), getenv('1Sc7e6YK7bWVeadJLOByKPpQjQgrOOND'));
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     echo "Database connection failed: " . $e->getMessage();
 }
